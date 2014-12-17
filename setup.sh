87cb891a9c410a82f6d8846775a4b2d9ebd2873d
Bo Ning Han <bo.ning.han@gmail.com>
2014-12-16 22:20:17 -0800

    fix ln

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 cd ~
 
-ln -s .bashrc dotfiles/.bashrc
-ln -s .tmux.conf dotfiles/.tmux.conf
-ln -s .gitconfig dotfiles/.gitconfig
+ln -s dotfiles/.bashrc .bashrc
+ln -s dotfiles/.tmux.conf .tmux.conf
+ln -s dotfiles/.gitconfig .gitconfig
 
 source .bashrc
 
-ln -s .vimrc dotfiles/.vimrc
+ln -s dotfiles/.vimrc .vimrc
 git clone https://github.com/gmarik/Vundle.vim.git .vim/bundle/Vundle.vim
 vim +PluginInstall
