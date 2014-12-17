040db93959a5588aa5dc9684246105f74328351f
Bo Ning Han <bo.ning.han@gmail.com>
2014-12-16 22:19:06 -0800

    all relative to home directory

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
-ln -s ~/.bashrc dotfiles/.bashrc
-ln -s ~/.tmux.conf dotfiles/.tmux.conf
-ln -s ~/.gitconfig dotfiles/.gitconfig
+cd ~
 
-source ~/.bashrc
+ln -s .bashrc dotfiles/.bashrc
+ln -s .tmux.conf dotfiles/.tmux.conf
+ln -s .gitconfig dotfiles/.gitconfig
 
-ln -s ~/.vimrc dotfiles/.vimrc
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+source .bashrc
+
+ln -s .vimrc dotfiles/.vimrc
+git clone https://github.com/gmarik/Vundle.vim.git .vim/bundle/Vundle.vim
 vim +PluginInstall
