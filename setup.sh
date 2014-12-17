69118b9b6d059c6c969a5dee697833aabcd9b103
Bo Ning Han <bo.ning.han@gmail.com>
2014-12-16 22:23:19 -0800

    make everything relative to

--- setup.sh
+++ setup.sh
@@ -1,11 +1,9 @@
-cd ~
+ln -s ~/dotfiles/.bashrc ~/.bashrc
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 
-ln -s dotfiles/.bashrc .bashrc
-ln -s dotfiles/.tmux.conf .tmux.conf
-ln -s dotfiles/.gitconfig .gitconfig
+source ~/.bashrc
 
-source .bashrc
-
-ln -s dotfiles/.vimrc .vimrc
-git clone https://github.com/gmarik/Vundle.vim.git .vim/bundle/Vundle.vim
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall
