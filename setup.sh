9970d5364f482d42a8d8f4240bf6e0f40626f73a
Bo Ning Han <bo.ning.han@gmail.com>
2014-12-16 22:17:28 -0800

    setup file

---
+++ setup.sh
@@ -0,0 +1,9 @@
+ln -s ~/.bashrc dotfiles/.bashrc
+ln -s ~/.tmux.conf dotfiles/.tmux.conf
+ln -s ~/.gitconfig dotfiles/.gitconfig
+
+source ~/.bashrc
+
+ln -s ~/.vimrc dotfiles/.vimrc
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall
